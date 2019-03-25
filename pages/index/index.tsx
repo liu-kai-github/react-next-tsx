db27f69075eee7747bcad4a261db484c929a4ae4
liu-kai-github <liu-kai-github@users.noreply.github.com>
2019-03-25 21:06:47 +0800

    添加 tdk

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,31 +1,22 @@
 import * as React from 'react';
 import Link from 'next/link'
 import './index.scss';
+import Head from "next/head";
 
 function HelloWorld() {
     return (
         <div>
-            Hello world
-            <p>scoped!</p>
-            <Link as={'/p/1'} href={'/post?id=1'}><span>Link</span></Link>
-            <style jsx>{`
-                p {
-                  color: blue;
-                }
-                div {
-                  background: red;
-                }
-                @media (max-width: 600px) {
-                  div {
-                    background: blue;
-                  }
-                }
-              `}</style>
-            <style global jsx>{`
-                body {
-                  background: black;
-                }
-            `}</style>
+            <Head>
+                <title>Hello World</title>
+                <meta name="description" content="简介内容"/>
+                <meta name="keywords" content="HTML,ASP,PHP,SQL"/>
+            </Head>
+            <h1>Hello world</h1>
+            <Link
+                as={'/p/1'}
+                // href={'/post?id=1'}
+                href={{pathname: '/post', query: {id: 1}}}
+            ><span>Link</span></Link>
         </div>
     )
 }
