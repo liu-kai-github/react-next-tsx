69c275d0a692a1a56bc97dfb4a3bca74f08c3493
liu-kai-github <liu-kai-github@users.noreply.github.com>
2019-03-25 18:54:47 +0800

    添加重定向

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
+const redirects = [
+    // {from: '/old-link-1', to: '/new-link-1'},
+    // {from: '/old-link-2', to: 'https://externalsite.com/new-link-2'},
+];
+
+
 app
     .prepare()
     .then(() => {
         const server = express();
+
+        redirects.forEach(({from, to, type = 301, method = 'get'}) => {
+            server[method](from, (req, res) => {
+                res.redirect(type, to)
+            })
+        });
 
         server.get('/p/:id', (req, res) => {
             const actualPage = '/post';
